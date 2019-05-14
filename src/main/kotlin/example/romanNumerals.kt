b8d2954ef6613c8c47f1f760a81fe816721951fb
Etienne GIROT <egirot@octo.com>
2019-05-14 15:43:59 +0200

    Extrapolated algorithm and reverse-algorithm

--- src/main/kotlin/example/romanNumerals.kt
+++ src/main/kotlin/example/romanNumerals.kt
@@ -1,10 +1,49 @@
 package example
 
-fun Int.toRomanNumeral() : String = when {
-    this == 40 -> "XL"
-    this >= 10 -> "X${(this-10).toRomanNumeral()}"
-    this == 9 -> "IX"
-    this >= 5 -> "V${(this-5).toRomanNumeral()}"
-    this == 4 -> "IV"
-    else -> (0 until this).joinToString("") { "I" }
+fun Int.toRomanNumeral(): String = Symbol.closestBelow(this)
+    .let { symbol ->
+        if (symbol != null) {
+            "$symbol${(this - symbol.decimalValue).toRomanNumeral()}"
+        } else {
+            ""
+        }
+    }
+
+private enum class Symbol(val decimalValue: Int) {
+    I(1),
+    IV(4),
+    V(5),
+    IX(9),
+    X(10),
+    XL(40),
+    L(50),
+    XC(90),
+    C(100),
+    CD(400),
+    D(500),
+    CM(900),
+    M(1000);
+
+    companion object {
+        fun closestBelow(value: Int) =
+            values()
+                .sortedByDescending { it.decimalValue }
+                .firstOrNull { value >= it.decimalValue }
+
+        fun highestStatingSymbol(value: String) =
+            values()
+            .sortedByDescending { it.decimalValue }
+            .firstOrNull { value.startsWith(it.name) }
+    }
+}
+
+fun String.toDecimal() : Int {
+    return Symbol.highestStatingSymbol(this)
+        .let{symbol ->
+        if (symbol != null) {
+            symbol.decimalValue + this.drop(symbol.name.length).toDecimal()
+        } else {
+            0
+        }
+    }
 }
--- src/test/kotlin/example/fromIntToRomanNumerals.kt
+++ src/test/kotlin/example/fromIntToRomanNumerals.kt
@@ -3,7 +3,7 @@ package example
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class RomanNumeralsTest : StringSpec({
+class FromIntToRomanNumeralsTest : StringSpec({
     "1 should be I" {
         1.toRomanNumeral() shouldBe("I")
     }
@@ -42,5 +42,41 @@ class RomanNumeralsTest : StringSpec({
     }
     "40 should be XL" {
         40.toRomanNumeral() shouldBe("XL")
+    }
+    "50 should be L" {
+        50.toRomanNumeral() shouldBe("L")
+    }
+    "57 should be LVII" {
+        57.toRomanNumeral() shouldBe("LVII")
+    }
+    "90 should be XC" {
+        90.toRomanNumeral() shouldBe("XC")
+    }
+    "98 should be XCVIII" {
+        98.toRomanNumeral() shouldBe("XCVIII")
+    }
+    "100 should be C" {
+        100.toRomanNumeral() shouldBe("C")
+    }
+    "194 should be CXCIV" {
+        194.toRomanNumeral() shouldBe("CXCIV")
+    }
+    "457 should be CDLVII" {
+        457.toRomanNumeral() shouldBe("CDLVII")
+    }
+    "500 should be D"{
+        500.toRomanNumeral() shouldBe("D")
+    }
+    "564 should be DLXIV" {
+        564.toRomanNumeral() shouldBe("DLXIV")
+    }
+    "1000 should be M" {
+        1000.toRomanNumeral() shouldBe("M")
+    }
+    "1998 should be MCMXCVIII" {
+        1998.toRomanNumeral() shouldBe("MCMXCVIII")
+    }
+    "2583 should be MCMXCVIII" {
+        2583.toRomanNumeral() shouldBe("MMDLXXXIII")
     }
 })
